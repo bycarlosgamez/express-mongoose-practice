37b46b0a73ddc3637683c6969faa74601c039ad0
Carlos Gámez <hardpx@gmail.com>
2022-10-17 19:38:23 -0500

    added details route

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get("/products", async (req, res) => {
   res.render("products/index", { products });
 });
 
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/details", { product });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
